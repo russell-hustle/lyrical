807df83e532e6c52e838a9e6623772d99742ca97
Will <willdickinson98@gmail.com>
2020-11-13 09:31:11 -0700

    restructure directory

--- extension.js
+++ extension.js
@@ -29,9 +29,9 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
 	deactivate
-}
+};
